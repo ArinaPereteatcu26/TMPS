b30295f68f6b664bda919775754e5f54ff406843
Arina <arinapereteatcu0@gmail.com>
2024-10-20 23:59:38 +0300

    Add Character Factory

---
+++ CreationalDesignPattern/CreationalDesignPattern/Client/PlayerInteraction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalDesignPattern.Client
+{
+    internal class PlayerInteraction
+    {
+    }
+}
---
+++ CreationalDesignPattern/CreationalDesignPattern/Factory/CharacterFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CreationalDesignPattern
+{
+    public class CharacterFactory
+    {
+        public Character CreateCharacter(string characterType)
+        {
+            switch (characterType)
+            {
+                case "Warrior":
+                    return new Warrior();
+                case "Mage":
+                    return new Wizard();
+                case "Archer":
+                    return new Archer();
+                default:
+                    throw new ArgumentException("Unknown character type.");
+            }
+        }
+    }
+
+}
