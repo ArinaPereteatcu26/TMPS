0a515219b1940619ab5314802a182d04b8bed9aa
Arina <arinapereteatcu0@gmail.com>
2024-10-20 23:56:18 +0300

    Add Base class Character

---
+++ CreationalDesignPattern/CreationalDesignPattern/Character.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalDesignPattern
+{
+    public abstract class Character
+    {
+        public abstract void Attack();
+    }
+
+}
---
+++ CreationalDesignPattern/CreationalDesignPattern/Wizard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalDesignPattern
+{
+    public class Wizard : Character
+    {
+        public override void Attack()
+        {
+            Console.WriteLine("Wizard casts a spell!");
+        }
+    }
+
+}
